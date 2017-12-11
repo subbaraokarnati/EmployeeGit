320dd0df11ae535f4ebf1ebb66ba0a6df5336118
KA376091 <KA376091@D-113074638.wipro.com>
2017-12-11 14:44:51 +0530

    In main method two lines is added in the sysout statement.

--- EmployeeGitDemo/src/com/empl/Employee.java
+++ EmployeeGitDemo/src/com/empl/Employee.java
@@ -5,5 +5,7 @@ public class Employee {
 	public static void main(String[] args) {
 		System.out.println("Employee Details:");
 		System.out.println("Employee Id:");
+		System.out.println("Employee Company:");
+		System.out.println("Employee Salary:");
 	}
 }
